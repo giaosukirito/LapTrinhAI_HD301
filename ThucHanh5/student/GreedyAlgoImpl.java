59447b78042d76fe2e56cae2728ea57342bd97c9
giaosukirito <21130612@st.hcmuaf.edu.vn>
2023-11-10 14:54:22 +0700

    push lab 5

--- ThucHanh5/student/GreedyAlgoImpl.java
+++ ThucHanh5/student/GreedyAlgoImpl.java
@@ -1,2 +1,31 @@
-package puzzle_8.student;public class GreedyAlgoIpml {
+package puzzle_8.student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class GreedyAlgoImpl implements IPuzzleAlgo{
+    @Override
+    public Node execute(Puzzle model) {
+        Queue<Node> frontier = new PriorityQueue<>(PuzzleUtils.HeuristicComparatorByH);
+        List<Node> explored = new ArrayList<>();
+        frontier.add(model.getInitialState());
+        while (!frontier.isEmpty()) {
+            Node current = frontier.poll();
+            if(current.getH() == 0) {
+                return current;
+            } else {
+                explored.add(current);
+                List<Node> children = model.getSuccessors(current);
+                for(Node child: children) {
+                    if(!explored.contains(child) && !frontier.contains(child)) {
+                        child.setG(current.getG()+1);
+                        frontier.add(child);
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
